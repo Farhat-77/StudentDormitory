9ae9208c6b223151db49012d80922cdfc518b9f3
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-15 11:38:45 +0600

    Добавьте файлы проекта.

---
+++ StudentDormitory/ListStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentDormitory
+{
+    public class ListStudent
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public int score;
+        public string name;
+        public int income;
+        public int group;
+
+        public override string ToString()
+        {
+            return (string.Format("Имя студента {0}; score{1}; Group № {2};Размер дохода на одного члена семьи: {3}", name, score, group, income));
+        }
+    }
+  
+}
---
+++ StudentDormitory/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentDormitory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество нуждающихся в общежитии:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            ListStudent[] nz = new ListStudent[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("{0} Нуждаюшиийся:", i + 1);
+                Console.WriteLine("Имя:");
+                nz[i].name = Console.ReadLine();
+                Console.WriteLine("Номер группы:");
+                nz[i].group = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Доход:");
+                Console.WriteLine("Средний балл:");
+                nz[i].score = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Доход:");
+                nz[i].income = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Введите количество мест в общежитии:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int mrot = 5000;
+            for (int i = 0; i < n; i++)
+            {
+                if (nz[i].income <= mrot)
+                    Console.WriteLine("{0} - Комната выдана!", nz[i]);
+
+                if (nz[i].income > mrot)
+                {
+
+                }
+            }
+        }
+    }
+}
